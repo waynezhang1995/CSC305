88225a2a3bd344f5eed90153f81157743b099b03
Wayne Zhang <waynezhangyuwei@Gmail.com>
2016-03-09 17:20:48 -0800

    Update main.cpp

--- A2-OpenGL Rendering/Part2/main.cpp
+++ A2-OpenGL Rendering/Part2/main.cpp
@@ -11,43 +11,24 @@ using namespace std;
 
 Canvas canvas;
 
+float vppos_x = 0;//cursor position; x coordinate
+float vppos_y = 0;//cursor position; y coordinate
+bool leftButtonPressed = false;//whether left button is pressed
+bool rightButtonPressed = false;//whether right button is pressed
+float lastx = vppos_x;//last cursor position; x coordinate
+float lasty = vppos_y;//last cursor position; y coordiante
+
 const char * vshader_square = "\
 #version 330 core \n \
 in vec3 vpoint;\
-uniform float dis;\
-uniform float rotateAngle;\
-uniform float rotateAngle1;\
+uniform mat4 UseMvp;\
 \
-void RotationMatrix(float dis, float rotateAngle, float rotateAngle1){\
-   vec3 EysPos = vec3(dis*sin(rotateAngle1)*sin(rotateAngle),dis*cos(rotateAngle1),dis*sin(rotateAngle1)*cos(rotateAngle));\
-   vec3 ViewUp = vec3(0,1,0);\
-   vec3 GazeDir = vec3(0,0,0);\
-   vec3 W = normalize(-(GazeDir-EysPos));\
-   vec3 U = normalize(cross(ViewUp,W));\
-   vec3 V = cross(W,U);\
-    mat4 viewtemp;\
-    viewtemp[0] = vec4(1,0,0,0);\
-    viewtemp[1] = vec4(0,1,0,0);\
-    viewtemp[2] = vec4(0,0,1,0);\
-    viewtemp[3] = vec4(-EysPos.x,-EysPos.y,-EysPos.z,1);\
-    mat4 viewrot;\
-    viewrot[0] = vec4(U.x,V.x,W.x,0);\
-    viewrot[1] = vec4(U.y,V.y,W.y,0);\
-    viewrot[2] = vec4(U.z,V.z,W.z,0);\
-    viewrot[3] = vec4(0,0,0,1);\
-    mat4 view;\
-    view = viewrot * viewtemp;\
-    vec4 point = vec4(vpoint,1);\
-    vec4 result = view * point;\
-    mat4 pes;\
-    pes[0] = vec4(1/result.z,0,0,0);\
-    pes[1] = vec4(0,1/result.z,0,0);\
-    pes[2] = vec4(0,0,1+50-50/result.z,0);\
-    pes[3] = vec4(0,0,0,1);\
-    gl_Position = pes * result;\
+void RotationMatrix(mat4 UseMvp){\
+    vec4 temp = UseMvp * vec4(vpoint,1);\
+    gl_Position = temp ;\
 }\
 void main(){\
-RotationMatrix(dis,rotateAngle,rotateAngle1);}";
+RotationMatrix(UseMvp);}";
 
 const char * fshader_square = "\
 #version 330 core \n \
@@ -55,7 +36,8 @@ out vec3 color;\
 void main(){color = vec3(0,45,2);}";
 
 const GLfloat vpoint[]={
-        -0.5f,-0.5f,-0.5f, // triangle 1 : begin
+
+        -0.5f,-0.5f,-0.5f, // triangle 1 : beg
         -0.5f,-0.5f, 0.5f,
         -0.5f, 0.5f, 0.5f, // triangle 1 : end
          0.5f, 0.5f,-0.5f, // triangle 2 : begin
@@ -92,6 +74,7 @@ const GLfloat vpoint[]={
         -0.5f, 0.5f, 0.5f,
          0.5f,-0.5f, 0.5f
 
+
 };
 
 float rotateAngle = 0 ; //The angle the camera currently rotated
@@ -101,9 +84,8 @@ float RotatingSpeed = 0.02;
 
 GLuint VertexArrayID = 0;
 GLuint ProgramID = 0;//the program we wrote
-GLuint RotationID = 0;
-GLuint RotationID1 = 0;
-GLuint disID = 0;
+GLuint MvpID = 0;
+
 
 float dis = 2.0;
 
@@ -124,20 +106,39 @@ void InitializeGL()
     glEnableVertexAttribArray(vpoint_id);
 
     glVertexAttribPointer(vpoint_id,3,GL_FLOAT,false,0,0);
-    disID = glGetUniformLocation(ProgramID,"dis");
-    RotationID = glGetUniformLocation(ProgramID,"rotateAngle");
-    RotationID1 = glGetUniformLocation(ProgramID,"rotateAngle1");
+    MvpID = glGetUniformLocation(ProgramID,"UseMvp");
+
 
 }
 
 void MouseMove(double x, double y)
 {
+    vppos_x = (float)(x) / 256 - 1;
+    vppos_y = 1 - (float)(y) / 256;
+    float dx = vppos_x- lastx;//record the last cursor postion x
+    float dy = vppos_y- lasty;//record the last cursor postion y
 
+    if(leftButtonPressed == true){//left button
+         rotateAngle += RotatingSpeed * -dx;//rotate camera left or right (dx > 0 --> right; dx < 0 --> left )
+         rotateAngle1 += RotatingSpeed * -dy;//rotate camera up or down (dy > 0 --> up; dy < 0 --> down)
+    }
+    if(rightButtonPressed == true){//right button
+         dis += dy*0.01;//move camera along the gaze direction (dis > 0 --> away; dis < 0 --> closer)
+    }
 }
 
 void MouseButton(MouseButtons mouseButton, bool press)
 {
-
+    if (mouseButton == LeftButton)
+    {
+        if (press == true) leftButtonPressed = true;
+        else leftButtonPressed = false;
+    }
+    if (mouseButton == RightButton)
+    {
+        if (press == true) rightButtonPressed = true;
+        else rightButtonPressed = false;
+    }
 }
 
 void KeyPress(char keychar)
@@ -147,16 +148,52 @@ void KeyPress(char keychar)
 
 void OnPaint()
 {
-       glClear(GL_COLOR_BUFFER_BIT);
+    /******************Setting up Mvp***********************/
+    Matrix4f Mvp;
+    Matrix4f viewtmp;
+    Matrix4f viewrot;
+    Matrix4f view;
+    Matrix4f Orth;
+    Matrix4f perspective;
+    Vector3f EysPos(dis*sin(rotateAngle1)*sin(rotateAngle),dis*cos(rotateAngle1),dis*sin(rotateAngle1)*cos(rotateAngle));//0,0,5 initially
+    Vector3f ViewUp(0,1,0);//up vector in Mv
+    Vector3f GazeDir(0,0,0);//gaze vector in Mv
 
+    /*Derive a coordinate system with origin e and uvw basis*/
+    Vector3f W = -(GazeDir-EysPos).normalized();
+    Vector3f U =(ViewUp.cross(W)).normalized();
+    Vector3f V = W.cross(U);
+
+    viewtmp<<1,0,0,-EysPos.x(),
+             0,1,0,-EysPos.y(),
+             0,0,1,-EysPos.z(),
+              0,0,0,1;
+
+    viewrot<<U.x(),U.y(),U.z(),0,
+             V.x(),V.y(),V.z(),0,
+             W.x(),W.y(),W.z(),0,
+             0,0,0,1;
+
+    view = viewrot*viewtmp;
+
+    perspective<<1,0,0,0,
+                 0,1,0,0,
+                 0,0,-1-50/-1,50,
+                 0,0,1/-1,0;
+
+    Orth<<1,0,0,0,
+          0,1,0,0,
+          0,0,2/(-1-(-50)),-(-1-50)/(-1+50),
+          0,0,0,1;
+
+    Mvp = Orth * perspective * view;
+    /*******************End**********************/
+    cout<<Mvp<<endl;
+
+    glClear(GL_COLOR_BUFFER_BIT);
     glUseProgram(ProgramID);
     glBindVertexArray(VertexArrayID);
-    glUniform1f(RotationID,rotateAngle);
-    glUniform1f(RotationID1,rotateAngle1);
-    glUniform1f(disID,dis);
-
-
-    //glUniform1f(RotationID,RotationAngle2);       //how to deal with second angle
+    glUniformMatrix4fv(MvpID,1,GL_FALSE,Mvp.data());
     /*
      * if leftbutton pressed --> RotationAngle
      * if rightbutton pressed --> RotationAngle2
@@ -178,7 +215,7 @@ int main(int, char **){
     canvas.SetMouseButton(MouseButton);
     canvas.SetKeyPress(KeyPress);
     canvas.SetOnPaint(OnPaint);
-    canvas.SetTimer(0.05, OnTimer);
+    //canvas.SetTimer(0.05, OnTimer);
     //Show Window
     canvas.Initialize(width, height, "OpenGL Intro Demo");
     //Do our initialization

8c16e91deabb0ff1f1475b351f8da8b39b6bc19e
Wayne Zhang <waynezhangyuwei@Gmail.com>
2016-03-11 21:23:51 -0800

    Added files via upload

---
+++ A2-OpenGL Rendering/Part2/data.h
@@ -0,0 +1,191 @@
+
+
+const GLfloat vpoint[]={
+    -0.5f ,-0.5f,-0.5f,
+    -0.5f, 0.5f, 0.5f,
+    -0.5f, 0.5f,-0.5f,
+
+    -0.5f,-0.5f,-0.5f, // triangle 1 : beg
+    -0.5f,-0.5f, 0.5f,
+    -0.5f, 0.5f, 0.5f, // triangle 1 : end
+     //back
+     0.5f, 0.5f,-0.5f, // triangle 2 : begin
+    -0.5f,-0.5f,-0.5f,
+    -0.5f, 0.5f,-0.5f, // triangle 2 : end
+
+     0.5f, 0.5f,-0.5f,
+     0.5f,-0.5f,-0.5f,
+    -0.5f,-0.5f,-0.5f,
+     //bottom
+     0.5f,-0.5f, 0.5f,
+    -0.5f,-0.5f,-0.5f,
+     0.5f,-0.5f,-0.5f,
+
+     0.5f,-0.5f, 0.5f,
+    -0.5f,-0.5f, 0.5f,
+    -0.5f,-0.5f,-0.5f,
+    //#7 frontal
+    -0.5f, 0.5f, 0.5f,
+    -0.5f,-0.5f, 0.5f,
+     0.5f,-0.5f, 0.5f,
+
+     0.5f, 0.5f, 0.5f,
+    -0.5f, 0.5f, 0.5f,
+     0.5f,-0.5f, 0.5f,
+     //right
+     0.5f, 0.5f, 0.5f,
+     0.5f,-0.5f,-0.5f,
+     0.5f, 0.5f,-0.5f,
+
+     0.5f,-0.5f,-0.5f,
+     0.5f, 0.5f, 0.5f,
+     0.5f,-0.5f, 0.5f,
+     //top
+     0.5f, 0.5f, 0.5f,
+     0.5f, 0.5f,-0.5f,
+    -0.5f, 0.5f,-0.5f,
+
+     0.5f, 0.5f, 0.5f,
+    -0.5f, 0.5f,-0.5f,
+    -0.5f, 0.5f, 0.5f,
+
+};
+
+const GLfloat vskypoint[]={
+        //left
+    -0.5f -2.0f,-0.5f,-0.5f,
+    -0.5f- 2.0f, 0.5f, 0.5f,
+    -0.5f- 2.0f, 0.5f,-0.5f,
+
+    -0.5f- 2.0f,-0.5f,-0.5f, // triangle 1 : beg
+    -0.5f- 2.0f,-0.5f, 0.5f,
+    -0.5f- 2.0f, 0.5f, 0.5f, // triangle 1 : end
+     //back
+     0.5f- 2.0f, 0.5f,-0.5f, // triangle 2 : begin
+    -0.5f- 2.0f,-0.5f,-0.5f,
+    -0.5f- 2.0f, 0.5f,-0.5f, // triangle 2 : end
+
+     0.5f- 2.0f, 0.5f,-0.5f,
+     0.5f- 2.0f,-0.5f,-0.5f,
+    -0.5f- 2.0f,-0.5f,-0.5f,
+     //bottom
+     0.5f- 2.0f,-0.5f, 0.5f,
+    -0.5f- 2.0f,-0.5f,-0.5f,
+     0.5f- 2.0f,-0.5f,-0.5f,
+
+     0.5f- 2.0f,-0.5f, 0.5f,
+    -0.5f- 2.0f,-0.5f, 0.5f,
+    -0.5f- 2.0f,-0.5f,-0.5f,
+    //#7 frontal
+    -0.5f- 2.0f, 0.5f, 0.5f,
+    -0.5f- 2.0f,-0.5f, 0.5f,
+     0.5f- 2.0f,-0.5f, 0.5f,
+
+     0.5f- 2.0f, 0.5f, 0.5f,
+    -0.5f- 2.0f, 0.5f, 0.5f,
+     0.5f- 2.0f,-0.5f, 0.5f,
+     //right
+     0.5f- 2.0f, 0.5f, 0.5f,
+     0.5f- 2.0f,-0.5f,-0.5f,
+     0.5f- 2.0f, 0.5f,-0.5f,
+
+     0.5f- 2.0f,-0.5f,-0.5f,
+     0.5f- 2.0f, 0.5f, 0.5f,
+     0.5f- 2.0f,-0.5f, 0.5f,
+     //top
+     0.5f- 2.0f, 0.5f, 0.5f,
+     0.5f- 2.0f, 0.5f,-0.5f,
+    -0.5f- 2.0f, 0.5f,-0.5f,
+
+     0.5f- 2.0f, 0.5f, 0.5f,
+    -0.5f- 2.0f, 0.5f,-0.5f,
+    -0.5f- 2.0f, 0.5f, 0.5f,
+
+};
+
+const GLfloat vtexcoord[] = {
+    //left
+    1, 0,
+    0, 1,
+    0, 0, //upper half of the square
+    1, 0,
+    1, 1,
+    0, 1, //upper half of the square
+    //back
+    1, 0,
+    0, 1,
+    1, 1, //upper half of the square
+    1, 0,
+    0, 0,
+    0, 1, //upper half of the square
+    //bottom
+    1, 1,
+    0, 0,
+    0, 1, //upper half of the square
+    1, 1,
+    1, 0,
+    0, 0, //upper half of the square
+    //frontal
+    0, 0,
+    1, 0,
+    1, 1, //upper half of the square
+    0, 1,
+    0, 0,
+    1, 1, //upper half of the square
+    //right
+    0, 0,
+    1, 1,
+    0, 1, //upper half of the square
+    1, 1,
+    0, 0,
+    1, 0, //upper half of the square
+    //top
+    1, 1,
+    0, 1,
+    0, 0, //upper half of the square
+    1, 1,
+    0, 0,
+    1, 0, //upper half of the square
+
+    1, 0,
+    0, 1,
+    0, 0, //upper half of the square
+    1, 0,
+    1, 1,
+    0, 1, //upper half of the square
+    //back
+    1, 0,
+    0, 1,
+    1, 1, //upper half of the square
+    1, 0,
+    0, 0,
+    0, 1, //upper half of the square
+    //bottom
+    1, 1,
+    0, 0,
+    0, 1, //upper half of the square
+    1, 1,
+    1, 0,
+    0, 0, //upper half of the square
+    //frontal
+    0, 0,
+    1, 0,
+    1, 1, //upper half of the square
+    0, 1,
+    0, 0,
+    1, 1, //upper half of the square
+    //right
+    0, 0,
+    1, 1,
+    0, 1, //upper half of the square
+    1, 1,
+    0, 0,
+    1, 0, //upper half of the square
+    //top
+    1, 1,
+    0, 1,
+    0, 0, //upper half of the square
+    1, 1,
+    0, 0,
+    1, 0, //upper half of the square
+}; //lower half of the square

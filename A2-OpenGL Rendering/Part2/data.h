b3bcc1f2df1ccbce60719a36c0f548db6e9b858b
Wayne Zhang <waynezhangyuwei@Gmail.com>
2016-03-14 19:23:47 -0700

    Update data.h

--- A2-OpenGL Rendering/Part2/data.h
+++ A2-OpenGL Rendering/Part2/data.h
@@ -97,113 +97,216 @@ const GLfloat vpoint[]={
      0.5f, 0.5f, 0.5f,
     -0.5f, 0.5f,-0.5f,
     -0.5f, 0.5f, 0.5f,
+
+    -0.5f ,-0.5f,-0.5f,
+    -0.5f, 0.5f, 0.5f,
+    -0.5f, 0.5f,-0.5f,
+
+    -0.5f,-0.5f,-0.5f, // triangle 1 : beg
+    -0.5f,-0.5f, 0.5f,
+    -0.5f, 0.5f, 0.5f, // triangle 1 : end
+     //back
+     0.5f, 0.5f,-0.5f, // triangle 2 : begin
+    -0.5f,-0.5f,-0.5f,
+    -0.5f, 0.5f,-0.5f, // triangle 2 : end
+
+     0.5f, 0.5f,-0.5f,
+     0.5f,-0.5f,-0.5f,
+    -0.5f,-0.5f,-0.5f,
+     //bottom
+     0.5f,-0.5f, 0.5f,
+    -0.5f,-0.5f,-0.5f,
+     0.5f,-0.5f,-0.5f,
+
+     0.5f,-0.5f, 0.5f,
+    -0.5f,-0.5f, 0.5f,
+    -0.5f,-0.5f,-0.5f,
+    //#7 frontal
+    -0.5f, 0.5f, 0.5f,
+    -0.5f,-0.5f, 0.5f,
+     0.5f,-0.5f, 0.5f,
+
+     0.5f, 0.5f, 0.5f,
+    -0.5f, 0.5f, 0.5f,
+     0.5f,-0.5f, 0.5f,
+     //right
+     0.5f, 0.5f, 0.5f,
+     0.5f,-0.5f,-0.5f,
+     0.5f, 0.5f,-0.5f,
+
+     0.5f,-0.5f,-0.5f,
+     0.5f, 0.5f, 0.5f,
+     0.5f,-0.5f, 0.5f,
+     //top
+     0.5f, 0.5f, 0.5f,
+     0.5f, 0.5f,-0.5f,
+    -0.5f, 0.5f,-0.5f,
+
+     0.5f, 0.5f, 0.5f,
+    -0.5f, 0.5f,-0.5f,
+    -0.5f, 0.5f, 0.5f,
 };
 
 
 const GLfloat CubeID[] = {
 
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2
 };
 
 
 
 const GLfloat vtexcoord[] = {
+
+    //LEFT
+    1.0f/2, 2.0f/3,
+    1.0f/4, 1.0f,
+    1.0f/2, 1.0f, //upper half of the square
+
+    1.0f/2, 2.0f/3,
+    1.0f/4, 2.0f/3,
+    1.0f/4, 1.0, //upper half of the square
+    //back
+    3.0f/4, 1.0f/3,
+    1.0f/2, 2.0f/3,
+    3.0f/4, 2.0f/3, //upper half of the square
+
+    3.0f/4, 1.0f/3,
+    1.0f/2, 1.0f/3,
+    1.0f/2, 2.0f/3,
+    //bottom
+    1.0f/2, 2.0f/3,
+    1.0f/4, 1.0f/3,
+    1.0f/4, 2.0f/3, //upper half of the square
+
+    1.0f/2, 2.0f/3,
+    1.0f/2, 1.0f/3,
+    1.0f/4, 1.0f/3,
+    //frontal
+    0, 2.0f/3,
+    1.0f/4, 2.0f/3,
+    1.0f/4, 1.0f/3, //upper half of the square
+
+    0, 1.0f/3,
+    0, 2.0f/3,
+    1.0f/4, 1.0f/3, //upper half of the square
+    //right
+    1.0f/4, 0,
+    1.0f/2, 1.0f/3,
+    1.0f/2, 0, //upper half of the square
+
+
+    1.0f/2, 1.0f/3,
+    1.0f/4, 0,
+    1.0f/4, 1.0f/3, //upper half of the square
+    //top
+    1, 1.0f/3,
+    3.0f/4, 1.0f/3,
+    3.0f/4, 2.0f/3, //upper half of the square
+
+    1, 1.0f/3,
+    3.0f/4, 2.0f/3,
+    1, 2.0f/3, //upper half of the square
     //left
-    1, 0,
-    0, 1,
-    0, 0, //upper half of the square
+    //LEFT
+    1.0f/2, 2.0f/3,
+    1.0f/4, 1.0f,
+    1.0f/2, 1.0f, //upper half of the square
 
-    1, 0,
-    1, 1,
-    0, 1, //upper half of the square
+    1.0f/2, 2.0f/3,
+    1.0f/4, 2.0f/3,
+    1.0f/4, 1.0, //upper half of the square
     //back
-    1, 0,
-    0, 1,
-    1, 1, //upper half of the square
+    3.0f/4, 1.0f/3,
+    1.0f/2, 2.0f/3,
+    3.0f/4, 2.0f/3, //upper half of the square
 
-    1, 0,
-    0, 0,
-    0, 1, //upper half of the square
+    3.0f/4, 1.0f/3,
+    1.0f/2, 1.0f/3,
+    1.0f/2, 2.0f/3,
     //bottom
-    1, 1,
-    0, 0,
-    0, 1, //upper half of the square
+    1.0f/2, 2.0f/3,
+    1.0f/4, 1.0f/3,
+    1.0f/4, 2.0f/3, //upper half of the square
 
-    1, 1,
-    1, 0,
-    0, 0, //upper half of the square
+    1.0f/2, 2.0f/3,
+    1.0f/2, 1.0f/3,
+    1.0f/4, 1.0f/3,
     //frontal
-    0, 0,
-    1, 0,
-    1, 1, //upper half of the square
+    0, 2.0f/3,
+    1.0f/4, 2.0f/3,
+    1.0f/4, 1.0f/3, //upper half of the square
 
-    0, 1,
-    0, 0,
-    1, 1, //upper half of the square
+    0, 1.0f/3,
+    0, 2.0f/3,
+    1.0f/4, 1.0f/3, //upper half of the square
     //right
-    0, 0,
-    1, 1,
-    0, 1, //upper half of the square
+    1.0f/4, 0,
+    1.0f/2, 1.0f/3,
+    1.0f/2, 0, //upper half of the square
 
-    1, 1,
-    0, 0,
-    1, 0, //upper half of the square
+
+    1.0f/2, 1.0f/3,
+    1.0f/4, 0,
+    1.0f/4, 1.0f/3, //upper half of the square
     //top
-    1, 1,
-    0, 1,
-    0, 0, //upper half of the square
+    1, 1.0f/3,
+    3.0f/4, 1.0f/3,
+    3.0f/4, 2.0f/3, //upper half of the square
 
-    1, 1,
-    0, 0,
-    1, 0, //upper half of the square
+    1, 1.0f/3,
+    3.0f/4, 2.0f/3,
+    1, 2.0f/3, //upper half of the square
 
-    1, 0,
-    0, 1,
-    0, 0, //upper half of the square
 
-    1, 0,
-    1, 1,
-    0, 1, //upper half of the square
+    //LEFT
+    1.0f/2, 2.0f/3,
+    1.0f/4, 1.0f,
+    1.0f/2, 1.0f, //upper half of the square
+
+    1.0f/2, 2.0f/3,
+    1.0f/4, 2.0f/3,
+    1.0f/4, 1.0, //upper half of the square
     //back
-    1, 0,
-    0, 1,
-    1, 1, //upper half of the square
+    3.0f/4, 1.0f/3,
+    1.0f/2, 2.0f/3,
+    3.0f/4, 2.0f/3, //upper half of the square
 
-    1, 0,
-    0, 0,
-    0, 1, //upper half of the square
-
+    3.0f/4, 1.0f/3,
+    1.0f/2, 1.0f/3,
+    1.0f/2, 2.0f/3,
     //bottom
-    1, 1,
-    0, 0,
-    0, 1, //upper half of the square
+    1.0f/2, 2.0f/3,
+    1.0f/4, 1.0f/3,
+    1.0f/4, 2.0f/3, //upper half of the square
 
-    1, 1,
-    1, 0,
-    0, 0, //upper half of the square
+    1.0f/2, 2.0f/3,
+    1.0f/2, 1.0f/3,
+    1.0f/4, 1.0f/3,
     //frontal
-    0, 0,
-    1, 0,
-    1, 1, //upper half of the square
+    0, 2.0f/3,
+    1.0f/4, 2.0f/3,
+    1.0f/4, 1.0f/3, //upper half of the square
 
-    0, 1,
-    0, 0,
-    1, 1, //upper half of the square
+    0, 1.0f/3,
+    0, 2.0f/3,
+    1.0f/4, 1.0f/3, //upper half of the square
     //right
-    0, 0,
-    1, 1,
-    0, 1, //upper half of the square
+    1.0f/4, 0,
+    1.0f/2, 1.0f/3,
+    1.0f/2, 0, //upper half of the square
 
-    1, 1,
-    0, 0,
-    1, 0, //upper half of the square
+
+    1.0f/2, 1.0f/3,
+    1.0f/4, 0,
+    1.0f/4, 1.0f/3, //upper half of the square
     //top
-    1, 1,
-    0, 1,
-    0, 0, //upper half of the square
+    1, 1.0f/3,
+    3.0f/4, 1.0f/3,
+    3.0f/4, 2.0f/3, //upper half of the square
 
-    1, 1,
-    0, 0,
-    1, 0, //upper half of the square
+    1, 1.0f/3,
+    3.0f/4, 2.0f/3,
+    1, 2.0f/3, //upper half of the square
 }; //lower half of the square

0ebce3ccf22f96fa67f88c2125f056669b027d7a
Wayne Zhang <waynezhangyuwei@Gmail.com>
2016-03-19 17:26:38 -0700

    Update data.h

--- A2-OpenGL Rendering/Part2/data.h
+++ A2-OpenGL Rendering/Part2/data.h
@@ -1,41 +1,4 @@
-#include "Canvas.h"
-#include <math.h>
-#include "Eigen/Dense"
-#include <iostream>
 
-unsigned int width = 1024; //window width
-unsigned int height = 1024; //window height
-
-using namespace Eigen;
-using namespace std;
-
-Canvas canvas;
-
-float vppos_x = 0;//cursor position; x coordinate
-float vppos_y = 0;//cursor position; y coordinate
-bool leftButtonPressed = false;//whether left button is pressed
-bool rightButtonPressed = false;//whether right button is pressed
-float lastx = vppos_x;//last cursor position; x coordinate
-float lasty = vppos_y;//last cursor position; y coordiante
-float rotateAngle = 0 ; //The angle the camera currently rotated
-                        //The angle between cube center and camera in a spherical coordinate
-float rotateAngle1 = M_PI * 0.5; //The angle the camera currently rotated
-float RotatingSpeed = 0.01; //rotate speed. Both camera and smaller cube share the same rotation speed
-float rot = M_PI * 0.5; //smaller cube orbiting angle.
-float spin = 0; //smaller cube self-rotating angle
-float far = -100.0f; //far plane
-float near = -1.0f; //near plane
-GLuint VertexArrayID = 0; //vertex array buffer ID
-GLuint ProgramID = 0;//the program we wrote
-GLuint MvpID = 0; //Mvp matrix ID
-GLuint rotID = 0; //rotation ID
-GLuint spinID = 0; //spin ID
-GLuint EyeID = 0; //Eye position ID
-GLuint tex_bindingpoint; //first texture binding point
-GLuint tex_bindingpointBG; //skybox texture binding point
-GLuint texobject; //cube texture
-GLuint texobjectBG; //skybox texture
-float dis = 3.0; //distance between camera and object
 
 const GLfloat vpoint[]={
 

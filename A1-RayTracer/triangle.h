631cc6e2fd29232300df1875de409f31fc8517e8
Wayne Zhang <waynezhangyuwei@Gmail.com>
2016-03-30 14:34:38 -0700

    Update triangle.h

--- A1-RayTracer/triangle.h
+++ A1-RayTracer/triangle.h
@@ -7,14 +7,17 @@ class triangle : public Object
     Vector3 Vertax3;
     Vector3 Vertax2;
     Vector3 Vertax1;
+    Vector3 TriangleNormal;
+    Vector3 TriangleColor;
     int flag;
 public:
-    triangle(Vector3 Vertax3_in, Vector3 Vertax2_in,Vector3 Vertax1_in,int flag_in)
+    triangle(Vector3 Vertax3_in, Vector3 Vertax2_in,Vector3 Vertax1_in,int flag_in, Vector3 TriangleNormal_in,Vector3 TriangleColor_in)
     {
         Vertax3 = Vertax3_in;
         Vertax2 = Vertax2_in;
         Vertax1 = Vertax1_in;
-
+        TriangleNormal = TriangleNormal_in;
+        TriangleColor = TriangleColor_in;
         flag = flag_in;
     }
     virtual Vector3 getCenter(){return Vertax3;}
@@ -25,29 +28,7 @@ public:
                            float *t, Vector3 *normal)
     {
          Vector3 trianglenormal;
-        if(flag == 100){
-            Vector3 tmp(0,1,0);
-            trianglenormal= tmp;
-        }
-        if(flag == 96){
-            Vector3 tmp(0,-1,0);
-            trianglenormal= tmp;
-        }
-        if(flag == 97){
-            Vector3 tmp(0,0,-1);
-            trianglenormal= tmp;
-        }
-        if(flag == 98){
-            Vector3 tmp(-1,0,0);
-            trianglenormal= tmp;
-        }
-        if(flag == 99){
-            Vector3 tmp(1,0,0);
-            trianglenormal= tmp;
-        }
-        //CrossProduct((Minus(Vertax3,Vertax1)),Minus(Vertax3,Vertax2));
-        //trianglenormal = Normalize(trianglenormal);
-        //printf("%d %d %d\n",trianglenormal.x,trianglenormal.y,trianglenormal.z);
+         trianglenormal = TriangleNormal;
         (*normal) = trianglenormal;
         Vector3 NminusP = Minus(Origin,Vertax3);  //(o-p)
         float tmp = -1 * DotProduct(NminusP,trianglenormal); //(o-p).n
@@ -63,11 +44,11 @@ public:
                 line2 = DotProduct(trianglenormal,CrossProduct(Minus(Vertax2,Vertax3),Minus(Intersectiontmp,Vertax3)));
                 line3 = DotProduct(trianglenormal,CrossProduct(Minus(Vertax1,Vertax2),Minus(Intersectiontmp,Vertax2)));
 
-            if(line1 > 0 && line2 > 0 && line3 > 0){
+            if(line1 >= 0 && line2 >= 0 && line3 >= 0){
 
                 return true;
             }
-            if(line1 < 0 && line2 < 0 && line3 < 0){
+            if(line1 <= 0 && line2 <= 0 && line3 <= 0){
 
                 return true;
             }
@@ -77,26 +58,9 @@ public:
         }
     }
 
-
-
     Vector3 addcolour(int flag,float PlaneDiffuseTerm,Vector3 PixelColour,int blackOrWhite){
 
-        if(flag == 99){ //left
-            Vector3 PixelDiffuseColour = MultiplyScalar(left, PlaneDiffuseTerm);
-            PixelColour = Add(PixelColour,PixelDiffuseColour);
-        }
-        if(flag == 98){ //right
-            Vector3 PixelDiffuseColour = MultiplyScalar(right, PlaneDiffuseTerm);
-            PixelColour = Add(PixelColour,PixelDiffuseColour);
-        }
-        if(flag == 97){ //back
-            Vector3 PixelDiffuseColour = MultiplyScalar(back, PlaneDiffuseTerm);
-            PixelColour = Add(PixelColour,PixelDiffuseColour);
-        }
-        if(flag == 96 ){ //top
-            Vector3 PixelDiffuseColour = MultiplyScalar(top, PlaneDiffuseTerm);
-            PixelColour = Add(PixelColour,PixelDiffuseColour);
-        }
+
         if(flag == 100){
             if (blackOrWhite== -1){
                 Vector3 PixelDiffuseColour = MultiplyScalar(White, PlaneDiffuseTerm);
@@ -107,6 +71,9 @@ public:
                 PixelColour = Add(PixelColour,PixelDiffuseColour);
                 //SetColor(shade, PixelColour);
             }
+        }else{
+            Vector3 PixelDiffuseColour = MultiplyScalar(TriangleColor, PlaneDiffuseTerm);
+            PixelColour = Add(PixelColour,PixelDiffuseColour);
         }
         return PixelColour;
     }
@@ -118,7 +85,7 @@ public:
              int blackOrWhite = -1;
              float result = abs((int)Surface.x/50%2);
              float result1 = abs((int)Surface.z/50%2);
-             //std::cout << result << std::endl;
+
              if(result == result1  ){
                  blackOrWhite = 1;
              }
@@ -138,9 +105,9 @@ public:
              float PlaneDiffuseTerm2 = DotProduct(Normal,PlaneLightVector2);
              for(int i =0;i<pObjectList.size();++i){     //calculate shadow for each sphere
                  if(pObjectList[i]->getflag() < 10 && flag != 96){
-                    float NewDiscriminant = shadow(tmp,pObjectList[i]->getCenter(),pObjectList[i]->getRadius(),Light);
-                    float NewDiscriminant2 = shadow(tmp,pObjectList[i]->getCenter(),pObjectList[i]->getRadius(),Light2);
-                    //std::cout<<NewDiscriminant<<std::endl;
+                    float NewDiscriminant = shadowSphere(tmp,pObjectList[i]->getCenter(),pObjectList[i]->getRadius(),Light);
+                    float NewDiscriminant2 = shadowSphere(tmp,pObjectList[i]->getCenter(),pObjectList[i]->getRadius(),Light2);
+
                     if(NewDiscriminant > 0){  //intersect to one of the three spheres --> Blocked !
                         if(PlaneDiffuseTerm > 0){
                             PixelColour = addcolour(flag,PlaneDiffuseTerm,PixelColour,blackOrWhite);
@@ -181,7 +148,7 @@ public:
                  return shade;
         }
 
-        float shadow(Vector3 planeintersection,Vector3 SphereCenter,float SphereRadius,Vector3 Light){
+        float shadowSphere(Vector3 planeintersection,Vector3 SphereCenter,float SphereRadius,Vector3 Light){
 
             Vector3 Camera = planeintersection;      //define a new camera point = ray-plane intersection point
             Vector3 NewDirection = Minus(Light,Camera);//ray-plane intersection point to light
@@ -195,6 +162,5 @@ public:
                 return -1;
             }
             return (SphereRadius*SphereRadius) - NewOCSqure + NewD_Dot_OC * NewD_Dot_OC;
-
         }
 };

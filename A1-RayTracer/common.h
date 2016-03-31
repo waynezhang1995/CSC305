c8dd3e7b44803880951ab00991ee562a42b4533d
Wayne Zhang <waynezhangyuwei@Gmail.com>
2016-03-30 18:44:15 -0700

    Update common.h

--- A1-RayTracer/common.h
+++ A1-RayTracer/common.h
@@ -47,8 +47,8 @@ void SetColor(Pixel & px, Vector3 CalculatedColor)
 Vector3 AmbientColour(20, 20, 20);
 Vector3 DiffuseColour(124, 250, 250);
 Vector3 BackgroundColor(8, 8, 8);
-Vector3 Light(100,900,500);
-Vector3 Light2(900,900,500);  //define second light source
+Vector3 Light(400,900,500);
+Vector3 Light2(600,900,500);  //define second light source
 Vector3 Camera(500,500, -800);
 Vector3 SpecularColourSphere(255,255,255);
 Vector3 White(255,255,255);

4d6ac70b5fabdc443862c542cfd33883baf54f9f
Wayne Zhang <waynezhangyuwei@Gmail.com>
2016-04-13 13:59:19 -0700

    Update main.cpp

--- A1-RayTracer/main.cpp
+++ A1-RayTracer/main.cpp
@@ -1,10 +1,13 @@
 #include "Image.h"
 #include "raytrace.h"
+#include <cstdlib>
+#include <ctime>
 //#include "raytracer.h"
 
 int main(int, char**){
 
     Image image(1000, 1000);
+    srand (static_cast <unsigned> (time(0)));
     //RayTrace(&image);     //old version
     RayTraceSphere(&image);
     image.show(" CSC 305 RayTracer Demo ");
